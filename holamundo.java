5a2d68bdc06df8d17f5b834ba9bb119684a713b4
elizabethtrevino <elizabethtrevino@thenorthcode.com>
2020-02-10 15:22:22 -0600

    added multiplication

--- holamundo.java
+++ holamundo.java
@@ -1,9 +1,12 @@
 import java.io.*;
 
+float a, b;
+
 class Hola{
     public static void main(String args[]) {
         System.out.println("ingresa opcion");
     }
+    public static float multiplicar(a, b){
+        return a*b;
+    }
 }
-
-// Comentario 
