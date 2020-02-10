9be5c437e55546d59e758f1b56e39b499c890273
elizabethtrevino <elizabethtrevino@thenorthcode.com>
2020-02-10 14:52:22 -0600

    version 2

--- holamundo.java
+++ holamundo.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 class Hola{
     public static void main(String args[]) {
-        System.out.println("Hola mundo");
+        System.out.println("es un programa en java");
     }
 }
 
