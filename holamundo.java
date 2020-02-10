45a94b3f117707aaa26a72436ed5f4f450f9a8e7
elizabethtrevino <elizabethtrevino@thenorthcode.com>
2020-02-10 15:15:17 -0600

    menu

--- holamundo.java
+++ holamundo.java
@@ -2,10 +2,7 @@ import java.io.*;
 
 class Hola{
     public static void main(String args[]) {
-        System.out.println("Hola mundo");
-        System.out.println("Adios mundo");
-        System.out.println("es un programa en java");
-        System.out.println("hello world");
+        System.out.println("ingresa opcion");
     }
 }
 
